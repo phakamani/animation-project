e4fbe1693725150d7caf05b98aa8b96d332ce2e7
phakamani1 <mkulisep@gmail.com>
2021-07-06 22:11:32 +0200

    Added the animation content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: {
+    '(document:keyup)': 'onDocumentKeyup($event)'
+  }
 })
 export class AppComponent {
   title = 'animation-project';
+  showLoadingMessage = false;
+  keyPressed = false;
+
+  onDocumentKeyup(event) {
+    if (event.keyCode === 122) {
+      event.preventDefault();
+      this.keyPressed = true;
+      setTimeout(() => {
+        this.showLoadingMessage = true;
+      }, 5000)
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
